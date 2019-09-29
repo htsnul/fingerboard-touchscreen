08c3804c61005991176e8686c65580af18276dbe
Ishimura Hitoshi <htsnul@gmail.com>
2019-09-29 07:42:18 +0100

    Changed the numbers of columns to be changable.

--- index.js
+++ index.js
@@ -1,91 +1,71 @@
-'use strict';
-
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-if (!window.AudioContext) {
-  window.AudioContext = webkitAudioContext;
-}
-
 var gAudioCtx = null;
 var gGainNode = null;
 var gTones = [];
+var columnCount = 0;
 
-var Tone = (function () {
-  function Tone(rowIndex, columnIndex) {
-    var _this = this;
-
-    _classCallCheck(this, Tone);
-
+class Tone {
+  constructor(rowIndex, columnIndex) {
     this.scale = [-5, -10, -14, -19, -24, -29][rowIndex] + columnIndex;
     var scaleInOctave = (this.scale + 48) % 12;
-    this.isBlackKey = [1, 4, 6, 9, 11].indexOf(scaleInOctave) != -1;
+    this.isBlackKey =  [1, 4, 6, 9, 11].indexOf(scaleInOctave) != -1;
     this.element = document.createElement('td');
     this.element.style.backgroundColor = this.isBlackKey ? '#eee' : '#fff';
-    this.element.ontouchstart = function (event) {
-      _this.start();
+    this.element.ontouchstart = (event) => {
+      this.start();
       event.preventDefault();
     };
-    this.element.ontouchend = function (event) {
-      _this.stop();
+    this.element.ontouchend = (event) => {
+      this.stop();
       event.preventDefault();
     };
     this.element.ontouchcancel = this.element.ontouchend;
-    this.element.onmousedown = function (event) {
-      _this.start();
+    this.element.onmousedown = (event) => {
+      this.start();
       event.preventDefault();
-    };
+    }
     document.querySelectorAll('tr')[rowIndex].appendChild(this.element);
   }
-
-  _createClass(Tone, [{
-    key: 'start',
-    value: function start() {
-      if (this.oscillator) {
-        return;
-      }
-      this.oscillator = gAudioCtx.createOscillator();
-      this.oscillator.type = 'square';
-      this.oscillator.frequency.value = 440 * Math.pow(2, this.scale / 12);
-      this.oscillator.connect(gGainNode);
-      this.oscillator.start();
-      this.element.style.backgroundColor = this.isBlackKey ? '#edd' : '#fee';
+  start() {
+    if (this.oscillator) {
+      return;
     }
-  }, {
-    key: 'stop',
-    value: function stop() {
-      if (!this.oscillator) {
-        return;
-      }
-      this.oscillator.stop();
-      this.oscillator = null;
-      this.element.style.backgroundColor = this.isBlackKey ? '#eee' : '#fff';
+    this.oscillator = gAudioCtx.createOscillator();
+    this.oscillator.type = 'square';
+    this.oscillator.frequency.value = 440 * Math.pow(2, this.scale / 12);
+    this.oscillator.connect(gGainNode);
+    this.oscillator.start();
+    this.element.style.backgroundColor = this.isBlackKey ? '#edd' : '#fee';
+  }
+  stop() {
+    if (!this.oscillator) {
+      return;
     }
-  }]);
+    this.oscillator.stop();
+    this.oscillator = null;
+    this.element.style.backgroundColor = this.isBlackKey ? '#eee' : '#fff';
+  }
+}
 
-  return Tone;
-})();
-
-onload = function () {
-  document.querySelector('button').onclick = function () {
-    gAudioCtx = new AudioContext();
-    gGainNode = gAudioCtx.createGain();
-    gGainNode.gain.value = 0.25;
-    gGainNode.connect(gAudioCtx.destination);
-    var y, x;
-    for (y = 0; y < 6; ++y) {
-      for (x = 0; x < 23; ++x) {
-        gTones.push(new Tone(y, x));
-      }
+function start() {
+  gAudioCtx = new AudioContext();
+  gGainNode = gAudioCtx.createGain();
+  gGainNode.gain.value = 0.25;
+  gGainNode.connect(gAudioCtx.destination);
+  columnCount = parseInt(document.querySelector('.column-count-div input:checked').value);
+  var y, x;
+  for (y = 0; y < 6; ++y) {
+    for (x = 0; x < columnCount; ++x) {
+      gTones.push(new Tone(y, x));
     }
-    document.querySelector('table').onmouseup = function (event) {
-      gTones.forEach(function (tone) {
-        return tone.stop();
-      });
-    };
-    document.querySelector('button').hidden = true;
-    document.querySelector('table').hidden = false;
+  }
+  document.querySelector('table').onmouseup = (event) => {
+    gTones.forEach(tone => tone.stop());
   };
+  document.querySelector('.setting-div').hidden = true;
+  document.querySelector('.fingerboard-table').hidden = false;
+}
+
+onload = () => {
+  document.querySelector('button').onclick = start;
 };
 
